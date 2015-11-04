e421a2841c67e7fc41b36b7d8a38a53179cec640
赖信涛 <laixintao1995@163.com>
2015-11-04 12:12:59 +0800

    表单结果可以保存到数据库

--- timetable/views.py
+++ timetable/views.py
@@ -1,8 +1,11 @@
-# Create your views here.
+# -*- coding: utf-8 -*-
 
 from django.shortcuts import render_to_response,render,get_object_or_404
 from django.template.context import RequestContext
 from forms import courseForm
+from django.http import HttpResponse
+from models import QAtime
+import time
 
 def publish(request):
     if request.method == 'GET':
@@ -14,7 +17,21 @@ def publish(request):
     else:
         form = courseForm(request.POST)
         if form.is_valid():
-            pass
+            course_name = form.cleaned_data['course_name']
+            time = form.cleaned_data['time']
+            room = form.cleaned_data['room']
+            max_people = form.cleaned_data['max_people']
+            new_time_table = QAtime()
+            new_time_table.courseName = course_name
+            new_time_table.time = time
+            new_time_table.max_people = max_people
+            new_time_table.room = room
+            localtime = time.strftime('%Y-%m-%d %H:%M:%S')
+            new_time_table.pubTime = localtime
+            new_time_table.save()
+            return HttpResponse(str(course_name)+'\n'+
+                                str(time)+'\n'+
+                                str(room)+'\n'+
+                                str(max_people))
         else:
-            pass
-        return render_to_response('publish.html',RequestContext(request,{'form':form,}))
+            return render_to_response('publish.html',RequestContext(request,{'form':form,}))
