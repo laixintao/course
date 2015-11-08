ff6c1576427ddb3015ebc368284aa8e383d7461c
赖信涛 <laixintao1995@163.com>
2015-11-08 22:24:19 +0800

    区分学生和老师的登录

--- accouts/admin.py
+++ accouts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from models import TeacherGroups
 # Register your models here.
+
+class TeacherGroupsAdmin(admin.ModelAdmin):
+    list_display = {
+        'username',
+    }
+
+admin.site.register(TeacherGroups)
--- accouts/models.py
+++ accouts/models.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+
 from django.db import models
 
 # Create your models here.
+class TeacherGroups(models.Model):
+    username = models.CharField(u'用户名',max_length=128)
+
+    def __unicode__(self):
+        return self.username
