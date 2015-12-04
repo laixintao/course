5988c754ef14f6ac5a09b1a4d4e950ba9580060e
赖信涛 <laixintao1995@163.com>
2015-12-04 11:34:59 +0800

    登入登出搞定

--- accouts/admin.py
+++ accouts/admin.py
@@ -1,10 +1,1 @@
 from django.contrib import admin
-from models import TeacherGroups
-# Register your models here.
-
-class TeacherGroupsAdmin(admin.ModelAdmin):
-    list_display = {
-        'username',
-    }
-
-admin.site.register(TeacherGroups)
